12068ab5f3f32a1027a4bba03dcabb82a221d7c2
Wallace <gpt@kong.vision>
2025-05-09 16:08:32 +0800

    修复布局

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,7 +1,6 @@
 import { AppSidebar } from "@/components/custom/app-sidebar";
 import { SidebarProvider } from "@/components/ui/sidebar";
 import { createRootRoute, Outlet } from "@tanstack/react-router";
-
 export const Route = createRootRoute({
   component: () => (
     <div className="min-h-screen bg-background">
@@ -13,7 +12,7 @@ export const Route = createRootRoute({
 
       <div className="flex h-[calc(100vh-5rem)]">
         <SidebarProvider className="min-h-0">
-          <div className="flex h-full">
+          <div className="flex h-full w-full">
             <AppSidebar />
             <main className="flex-1 overflow-y-auto">
               <div className="h-full p-6">
