b24fbed7f0b9a1fdab0234e1a2df97ce1e42f219
gptkong <gptkong>
2025-06-06 11:07:50 +0800

    增加部分postcss支持

--- src/routes/about.tsx
+++ src/routes/about.tsx
@@ -1,9 +1,13 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute } from "@tanstack/react-router";
 
-export const Route = createFileRoute('/about')({
+export const Route = createFileRoute("/about")({
   component: RouteComponent,
-})
+});
 
 function RouteComponent() {
-  return <div>Hello "/about"!</div>
+  return (
+    <div className="overflow-auto w-full font-bold text-transparent bg-clip-text bg-gradient-to-r from-red-500 to-amber-500">
+      Hello "/about"!
+    </div>
+  );
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,25 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import path from "path"
-import tailwindcss from "@tailwindcss/vite"
-import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
+import tailwindcss from "@tailwindcss/vite";
+import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
+import autoprefixer from "autoprefixer";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
-    TanStackRouterVite({ target: 'react', autoCodeSplitting: true }),
+    TanStackRouterVite({ target: "react", autoCodeSplitting: true }),
     react(),
-    tailwindcss()
+    tailwindcss(),
   ],
+  css: {
+    postcss: {
+      plugins: [autoprefixer({})],
+    },
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+});
