27c8041ce9aefbaf3ab44cd5a242ad8727fdcd3d
gptkong <gptkong>
2025-06-10 10:14:53 +0800

    add autoprefixer

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import tailwindcss from "@tailwindcss/vite";
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
+import autoprefixer from "autoprefixer";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -11,6 +12,11 @@ export default defineConfig({
     react(),
     tailwindcss(),
   ],
+  css: {
+    postcss: {
+      plugins: [autoprefixer({})],
+    },
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
